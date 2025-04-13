4dfce3da84e409597568c1ab082ab03f758c0878
fainnerramirez <fainnerramirez@gmail.com>
2025-04-13 11:31:39 -0500

    Mejoras en el ejercicio #2

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -29,24 +29,30 @@ public class Ejercicio2 {
         int option = 0;
 
         do {
-            System.out.println("Ingrese la cantidad a comprar del café: ");
-            amountProductOne = sc.nextInt();
-            System.out.println("Ingrese la cantidad a comprar del jugo: ");
-            amountProductTwo = sc.nextInt();
-            System.out.println("Ingrese la cantidad a comprar del empanadas: ");
-            amountProductThree = sc.nextInt();
+            System.out.println("1. Comprar Productos");
+            System.out.println("0. Salir");
+            option = sc.nextInt();
+            sc.nextLine();
 
-            totalToPay = (amountProductOne * priceProductOne) + (amountProductTwo * priceProductTwo) + (amountProductThree * priceProductThree);
+            if (option == 1) {
+                System.out.println("Ingrese la cantidad a comprar del " + productOne + ": ");
+                amountProductOne = sc.nextInt();
+                System.out.println("Ingrese la cantidad a comprar del jugo " + productTwo + ": " );
+                amountProductTwo = sc.nextInt();
+                System.out.println("Ingrese la cantidad a comprar del empanadas " + productThree + ": ");
+                amountProductThree = sc.nextInt();
+            } else if (option == 0) {
+                totalToPay = (amountProductOne * priceProductOne) + (amountProductTwo * priceProductTwo) + (amountProductThree * priceProductThree);
 
-            if(totalToPay > valueExeceeded) {
-                totalToPay = totalToPay - (totalToPay * discount);
+                if (totalToPay > valueExeceeded) {
+                    totalToPay = totalToPay - (totalToPay * discount);
+                }
+
+                System.out.println("La cantidad del café es de: " + amountProductOne);
+                System.out.println("La cantidad del jugo es de: " + amountProductTwo);
+                System.out.println("La cantidad del empanadas es de: " + amountProductThree);
+                System.out.println("El total a pagar es de: " + totalToPay);
             }
-
-            System.out.println("La cantidad del café es de: " + amountProductOne);
-            System.out.println("La cantidad del jugo es de: " + amountProductTwo);
-            System.out.println("La cantidad del empanadas es de: " + amountProductThree);
-            System.out.println("El total a pagar es de: " + Math.round(totalToPay));
-
         } while (option != 0);
 
     }
