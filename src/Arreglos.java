af8b21eb3d6470dfa5a0ed6c6fc74cbcfd320b28
fainnerramirez <fainnerramirez@gmail.com>
2025-04-15 22:29:03 -0500

    coment java

--- src/Arreglos.java
+++ src/Arreglos.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Arreglos {
+    //arreglos en java
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int nElements;
